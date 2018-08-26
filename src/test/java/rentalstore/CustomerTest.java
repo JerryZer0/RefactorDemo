063a56adb66ac1eb2a0894d0f4d71b17ca9fc869
Jerry <jerry.zhang@oocl.com>
2018-08-26 14:54:37 +0800

    [Jerry]add test case as the exmple rent two films

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -69,5 +69,20 @@ public class CustomerTest {
                 + "On this rental you earned <EM>1</EM> frequent renter points<P>", receipt);
     }
 
+    @Test
+    public void should_return_the_HTML_result_as_name_is_Jerry_and_rent_two_films_for_one_day() {
+        Movie movie1 = new Movie("Roman Holiday", 1);
+        Movie movie2 = new Movie("Titanic", 1);
+        Rental rental1 = new Rental(movie1, 1);
+        Rental rental2 = new Rental(movie2, 1);
+        customer.addRental(rental1);
+        customer.addRental(rental2);
+        String receipt = customer.statement();
 
+        assertEquals("<H1>Rentals for <EM>Jerry</EM></H1><P>\n"
+                + "Roman Holiday\t3.0<BR>\n"
+                + "Titanic\t3.0<BR>\n"
+                + "<P>You owe<EM>6.0</EM><P>\n"
+                + "On this rental you earned <EM>2</EM> frequent renter points<P>", receipt);
+    }
 }
