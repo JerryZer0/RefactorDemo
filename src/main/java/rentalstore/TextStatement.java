4bdfdbd859704208c93f0ad2d1f250c29bb0c1e9
Jerry <jerry.zhang@oocl.com>
2018-08-26 16:31:35 +0800

    [Jerry] add the base class Statement

--- src/main/java/rentalstore/TextStatement.java
+++ src/main/java/rentalstore/TextStatement.java
@@ -2,73 +2,29 @@ package rentalstore;
 
 import java.util.Enumeration;
 
-public class TextStatement {
+public class TextStatement extends Statement{
 
     private Customer customer;
 
     public TextStatement(Customer customer) {
+        super(customer);
         this.customer = customer;
     }
 
-    String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Enumeration<Rental> rentals = customer.getRentals().elements();
-        String result = getResult();
-        while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
-            Rental each = rentals.nextElement();
-
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDayRented() > 2) {
-                        thisAmount += (each.getDayRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDayRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDayRented() > 3) {
-                        thisAmount += (each.getDayRented() - 3) * 1.5;
-                    }
-                    break;
-            }
-
-            //add frequent renter points
-            frequentRenterPoints++;
-            //add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1) {
-                frequentRenterPoints++;
-            }
-
-            //show figures for this rental
-            result += getBody(thisAmount, each);
-            totalAmount += thisAmount;
-        }
-
-        //add footer lines
-        result = getFooter(totalAmount, frequentRenterPoints, result);
-        return result;
-    }
-
-    private String getResult() {
+    public String getHeader() {
         return "Rentals for " + customer.getName() + "\n";
     }
 
-    private String getBody(double thisAmount, Rental each) {
+    public String getBody(double thisAmount, Rental each) {
         return each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
     }
 
-    private String getFooter(double totalAmount, int frequentRenterPoints, String result) {
-        result += "You owe "
+    public String getFooter(double totalAmount, int frequentRenterPoints) {
+        return "You owe "
                 + String.valueOf(totalAmount)
                 + "\n"
                 + "On this rental you earned "
                 + String.valueOf(frequentRenterPoints)
                 + " frequent renter points";
-        return result;
     }
 }
